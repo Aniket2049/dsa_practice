d0fb49be7a0a32c02af1c87a45568619838fb9f7
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2024-10-06 00:28:39 +0530

    add merge two sorted lists

---
+++ src/linkedlist/MergeSortedLists.java
@@ -0,0 +1,86 @@
+package linkedlist;
+
+// program to merge two sorted lists into one sorted
+public class MergeSortedLists {
+
+	// utility function which prints from a node
+	public static void printList(ListNode head) {
+		while (head != null) {
+			System.out.print(head.data + " ");
+			head = head.next;
+		}
+	}
+
+	// take two head pointers and compare values. push next node into third one
+	// according to sort logic. at end check if either of list still has
+	// elements, if they do append to third list
+	public static ListNode Solution1(ListNode head1, ListNode head2) {
+		ListNode head = new ListNode(0);
+		ListNode current = head;
+
+		while (head1 != null && head2 != null) {
+			if (head1.data <= head2.data) {
+				current.next = head1;
+				head1 = head1.next;
+				current = current.next;
+			} else {
+				current.next = head2;
+				head2 = head2.next;
+				current = current.next;
+			}
+		}
+
+		if (head1 != null)
+			current.next = head1;
+		else if (head2 != null)
+			current.next = head2;
+
+		return head.next;
+	}
+
+	// Same as solution1 except uses recursion
+	public static ListNode Solution2(ListNode head1, ListNode head2) {
+		if (head1 == null)
+			return head2;
+		if (head2 == null)
+			return head1;
+
+		ListNode head = null;
+
+		if (head1.data <= head2.data) {
+			head = head1;
+			head.next = Solution2(head1.next, head2);
+		} else {
+			head = head2;
+			head.next = Solution2(head1, head2.next);
+		}
+
+		return head;
+
+	}
+
+	public static void main(String[] args) {
+		LinkedList list1 = new LinkedList();
+		LinkedList list2 = new LinkedList();
+
+		for (int i = 0; i < 10; i++) {
+			if (i % 2 == 0)
+				list1.insertAtEnd(i);
+			else
+				list2.insertAtEnd(i);
+		}
+
+		System.out.println("Original Linked List : " + list1.toString());
+		System.out.println("Original Linked List : " + list2.toString());
+
+		// ---------- problem setup done ------------
+
+		System.out.println("\n--- Solution 1 ---");
+		printList(Solution1(list1.head, list2.head));
+		
+		System.out.println("\n--- Solution 1 ---");
+		printList(Solution2(list1.head, list2.head));
+
+	}
+
+}
