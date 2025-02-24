3d46d691b77d432af0cc60c284a312dbc50a818a
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-02-24 22:50:09 +0530

    add mirror a tree

---
+++ src/binarytree/_10_MirrorATree.java
@@ -0,0 +1,58 @@
+package binarytree;
+
+// create a mirror tree of given tree
+public class _10_MirrorATree {
+
+	public TreeNode mirrorATree(TreeNode root) {
+		TreeNode temp;
+		if (root != null) {
+			mirrorATree(root.left);
+			mirrorATree(root.right);
+			temp = root.left;
+			root.left = root.right;
+			root.right = temp;
+		}
+
+		return root;
+	}
+
+	public static void main(String[] args) {
+		// Constructing binary tree.
+		//
+		//             1
+		//            / \
+		//           /   \
+		//          2     3
+		//         / \   / \
+		//        /   \ /   \
+		//       4    5 6    7
+		TreeNode root = new TreeNode(1);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(3);
+		root.left.left = new TreeNode(4);
+		root.left.right = new TreeNode(5);
+		root.right.left = new TreeNode(6);
+		root.right.right = new TreeNode(7);
+
+	}
+
+	static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+}
